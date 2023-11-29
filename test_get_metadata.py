4b0570b732f977ff36993c8582a689728a7ccd63
Jonas Lenz <jonaspunktlenz@gmail.com>
2023-11-29 15:57:16 +0100

    put metadataretriever in simple functions, add first test

---
+++ test_get_metadata.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from get_metadata import *
+
+# start with a given DOI
+#doi = "10.14454/FXWS-0523"
+#doi = "10.5281/zenodo.6654150"
+#doi = "10.5281/zenodo.10210062"
+doi = "10.5281/zenodo.10209718"
+#doi = "10.5281/zenodo.10210061"
+
+def test_doi_ra1():
+    assert doi_ra("10.5281/zenodo.10209718") == "DataCite"
+
+def test_doi_ra2():
+    assert doi_ra("10.5281/zenodo.10209718", meta="True") == \
+    [{'DOI': '10.5281/zenodo.10209718', 'RA': 'DataCite'}]
