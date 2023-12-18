8cc770f9b6206bb011ce8a350484dedd0bc9ea33
jandevaty <jandevaty@hotmail.com>
2023-12-18 16:05:39 +0100

    Establish relationships module

---
+++ src/soilpulse/relationships.py
@@ -0,0 +1,16 @@
+# coding = utf-8
+# -*- coding: utf-8 -*-
+
+class Relationship():
+    """
+    Defines the very basic relation between entities
+    """
+
+    def __init__(self):
+        # relationship type
+        self.type = None
+        # subject A of the relationship
+        self.a = None
+        # subject B of the relationship
+        self.b = None
+        pass
