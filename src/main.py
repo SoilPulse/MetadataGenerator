dbe0de5a532f9fc39608d0baacdfb20fada623ba
jandevaty <jandevaty@hotmail.com>
2024-01-10 00:10:07 +0100

    Changes in metadata scheme, introduction of entity manager. Can't make it work because of import problems ... so untested

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from soilpulse.resource_management import ResourceManager
+from soilpulse.metadatascheme import *
+from soilpulse.exceptions import MetadataSchemeException
+
+if __name__ == "__main__":
+    resource = ResourceManager
+    em = EntityManager
+
+    print("hello SoilPulse")
---
+++ src/soilpulse/exceptions.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Jan Devátý
+"""
+
+class MetadataSchemeException(Exception):
+    """
+    This exception is raised when there's some inconsistency in the resource's metadata structure
+    """
+    def __init__(self, message):
+        self.message = "Metadata scheme error:\n" + message
