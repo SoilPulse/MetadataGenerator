35de5ff0facc0c5b36bb5fe065be51a93b4ab265
jonaslenz <jonaspunktlenz@gmail.com>
2024-07-24 11:50:07 +0200

    get concepts of agrovoc and allow selection

--- src/webapp/SoilPulse_middle.py
+++ src/webapp/SoilPulse_middle.py
@@ -50,8 +50,83 @@ def _create_tree(folder):
     return sub_folder_dic
 
 
+def _update_agrovoc_concept_dump():
+    """Get the list of all concepts of agrovoc to a local dict and pickle this.
+
+    Agrovoc currently includes < 44000 concepts, so limit of 100000 concepts
+    is sufficient.
+    the python sparql wrapper is described there:
+        https://sparqlwrapper.readthedocs.io/en/latest/main.html#command-line-script
+    the agrovoc sparql endpoint with examples there:
+        https://agrovoc.fao.org/sparql
+    I build upon the example while deleting date infos:
+        "Select all concepts added since date X (e.g. 31/12/2020), URI and EN prefLabel."
+    """
+    from SPARQLWrapper import SPARQLWrapper, JSON
+    import pickle
+
+    sparql = SPARQLWrapper(
+        "https://agrovoc.fao.org/sparql/"
+    )
+    sparql.setReturnFormat(JSON)
+
+    sparql.setQuery("""
+                    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+                    PREFIX skosxl: <http://www.w3.org/2008/05/skos-xl#>
+                    SELECT ?concept ?label
+                    WHERE {
+                        ?concept a skos:Concept .
+                        OPTIONAL {
+                            ?concept skosxl:prefLabel ?xEnLabel .
+                            ?xEnLabel skosxl:literalForm ?label .
+                            }
+                        FILTER(lang(?label) = 'en')
+                        }
+                    LIMIT 100000
+                    """
+                    )
+    ret = sparql.queryAndConvert()
+    with open("agrov.dump", 'wb') as handle:
+        pickle.dump(ret, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def _get_agrovoc_dump():
+    import pickle
+
+    if not os.path.isfile("agrov.dump"):
+        _update_agrovoc_concept_dump()
+    with open("agrov.dump", 'rb') as handle:
+        ret = pickle.load(handle)
+    optio = {r["label"]["value"]:
+             r["concept"] for r in ret["results"]["bindings"]}
+    return optio
+
+
+def _modify_agrovoc_concept(container):
+    optio = _get_agrovoc_dump()
+    keys = list(optio.keys())
+    if "agrovoc" in container:
+        index = keys.index("soil organic matter")
+    else:
+        index = None
+    agrovoc = st.selectbox("AGROVOC label",
+                           keys,
+                           help="Agrovoc is a controled vocabulary.\
+                               By assigning these vocabularies to your\
+                               data, a common understanding of the meaning\
+                               can be achieved.",
+                           index=index,
+                           placeholder="Choose an AGROVOC concept.")
+    if agrovoc:
+        st.write(agrovoc)
+        st.write(
+            "https://agrovoc.fao.org/browse/agrovoc/en/page/?uri=" +
+            optio[agrovoc]["value"])
+
+
 def _show_container_content(container):
     st.write(container)
+    _modify_agrovoc_concept(container)
     pass
 
 
