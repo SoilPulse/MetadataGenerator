caf2ef763ad63865dd4bb2bdcce216dbce0ef418
jonaslenz <38277721+jonaslenz@users.noreply.github.com>
2024-09-25 23:34:55 +0200

    Reimport issue?

--- src/webapp/datapackage_handling.py
+++ src/webapp/datapackage_handling.py
@@ -21,119 +21,119 @@ if 'pipe' not in st.session_state:
 if 'counter' not in st.session_state:
     st.session_state.counter = 0
 
-
-st.title("frictionless data manipulation")
+def main():
+    st.title("frictionless data manipulation")
 
 # expecting data package parsed from SoilPulse
 # manipulate descriptor
-basepath = st.session_state.localproject.temp_dir
-pipe_path = basepath + "/pipe.txt"
-zenodo_path = basepath + "/zenodometa.json"
-descriptor_path = basepath + "/package.json"
+    basepath = st.session_state.localproject.temp_dir
+    pipe_path = basepath + "/pipe.txt"
+    zenodo_path = basepath + "/zenodometa.json"
+    descriptor_path = basepath + "/package.json"
 
-with st.sidebar:
-    table = st.radio("Select resource table of this dataset package.", [x.name for x in st.session_state.package.resources])
-    st.warning("add package top level view here?")
+    with st.sidebar:
+        table = st.radio("Select resource table of this dataset package.", [x.name for x in st.session_state.package.resources])
+        st.warning("add package top level view here?")
 
-if len(st.session_state.package.resources) <= 0:
-    st.warning("There are no resources added by now, please select relevant resources from available files.")
-    st.stop()
-else:
-    with st.expander("original description"):
-        st.json(st.session_state.package.to_descriptor())
-    if st.button("save descriptor"):
-        with open(descriptor_path, "w") as file:
-            file.write(
-                str(st.session_state.package.to_descriptor())
-                )
-
-with st.expander("resource *"+table+"* before pipe"):
-    st.write(st.session_state.package.get_resource(table).to_pandas())
-
-with st.expander("modify pipeline"):
-    st.warning("Add user friendly interface (EGU Demo adaptation) to create steps in here!")
-    if st.toggle("(re)load existing pipe"):
-        with open(pipe_path) as pipe:
-            apipe = pipe.read()
+    if len(st.session_state.package.resources) <= 0:
+        st.warning("There are no resources added by now, please select relevant resources from available files.")
+        st.stop()
     else:
-        apipe = st.session_state.pipe
-    apipe = st.text_area("modify pipeline", key = "modpipe"+str(st.session_state.counter), value = apipe)
+        with st.expander("original description"):
+            st.json(st.session_state.package.to_descriptor())
+        if st.button("save descriptor"):
+            with open(descriptor_path, "w") as file:
+                file.write(
+                    str(st.session_state.package.to_descriptor())
+                    )
 
-if not st.toggle("Run Pipeline steps"):
-    st.stop()
+    with st.expander("resource *"+table+"* before pipe"):
+        st.write(st.session_state.package.get_resource(table).to_pandas())
 
-with st.expander("resource transformed"):
-    if st.button("apply new Pipe", disabled = st.session_state.pipe == apipe):
-        try:
-            pipeline = Pipeline(steps= eval(apipe))
-            st.write(pipeline)
-            st.session_state.package.transform(pipeline)
-            st.session_state.pipe = apipe
-            st.session_state.counter += 1
-            st.rerun()
-        except Exception as e:
-            st.warning("Invalid pipe:")
-            st.warning(e)
-            st.stop()
+    with st.expander("modify pipeline"):
+        st.warning("Add user friendly interface (EGU Demo adaptation) to create steps in here!")
+        if st.toggle("(re)load existing pipe"):
+            with open(pipe_path) as pipe:
+                apipe = pipe.read()
+        else:
+            apipe = st.session_state.pipe
+        apipe = st.text_area("modify pipeline", key = "modpipe"+str(st.session_state.counter), value = apipe)
+
+    if not st.toggle("Run Pipeline steps"):
+        st.stop()
+
+    with st.expander("resource transformed"):
+        if st.button("apply new Pipe", disabled = st.session_state.pipe == apipe):
+            try:
+                pipeline = Pipeline(steps= eval(apipe))
+                st.write(pipeline)
+                st.session_state.package.transform(pipeline)
+                st.session_state.pipe = apipe
+                st.session_state.counter += 1
+                st.rerun()
+            except Exception as e:
+                st.warning("Invalid pipe:")
+                st.warning(e)
+                st.stop()
     #st.session_state.package.resources[0]
-    a = st.session_state.package.get_resource(table).to_pandas()
-    if "time" in a.index.names:
-        a.index = a.index.set_levels( a.index.levels[1].astype("timedelta64[s]"), level = 1)
-    a = a.reset_index()
-    st.write(a.head())
-with st.expander("resource visualisation"):
-    options = st.multiselect("choose relevant columns",
+        a = st.session_state.package.get_resource(table).to_pandas()
+        if "time" in a.index.names:
+            a.index = a.index.set_levels( a.index.levels[1].astype("timedelta64[s]"), level = 1)
+        a = a.reset_index()
+        st.write(a.head())
+    with st.expander("resource visualisation"):
+        options = st.multiselect("choose relevant columns",
                              options = a.columns
                              )
-    colsel = st.columns(3)
-    if len(options)>0:
-        with colsel[0]:
-            sely = st.selectbox("select column for y", options=options)
-        with colsel[1]:
-            selx = st.selectbox("select column for x", options=options)
-        with colsel[2]:
-            selc = st.selectbox("select column for color", options=options)
+        colsel = st.columns(3)
+        if len(options)>0:
+            with colsel[0]:
+                sely = st.selectbox("select column for y", options=options)
+            with colsel[1]:
+                selx = st.selectbox("select column for x", options=options)
+            with colsel[2]:
+                selc = st.selectbox("select column for color", options=options)
 
 
-    if st.toggle("chart"):
-        try:
-            st.scatter_chart(a, y = sely, x = selx, color=selc)
-        except Exception as e:
-            st.warning("No chart possible.")
-            st.warning(e)
-    if st.toggle("table"):
-        try:
-            mylist = [selc, sely, selx]
-            mylist = list(dict.fromkeys(mylist))
-            st.write(a[mylist])
-        except Exception as e:
-            st.warning("No table possible.")
-            st.warning(e)
+        if st.toggle("chart"):
+            try:
+                st.scatter_chart(a, y = sely, x = selx, color=selc)
+            except Exception as e:
+                st.warning("No chart possible.")
+                st.warning(e)
+        if st.toggle("table"):
+            try:
+                mylist = [selc, sely, selx]
+                mylist = list(dict.fromkeys(mylist))
+                st.write(a[mylist])
+            except Exception as e:
+                st.warning("No table possible.")
+                st.warning(e)
 
-with st.expander("resource validation"):
-    report = st.session_state.package.get_resource(table).validate()
-    if st.button("show validate"):
-        st.write(report)
-        st.json(st.session_state.package.validate().to_json())
+    with st.expander("resource validation"):
+        report = st.session_state.package.get_resource(table).validate()
+        if st.button("show validate"):
+            st.write(report)
+            st.json(st.session_state.package.validate().to_json())
 
-st.warning("hard coded deposition ID")
-if not 'deposition_id' in st.session_state:
-    st.session_state.deposition_id = 109575
+    st.warning("hard coded deposition ID")
+    if not 'deposition_id' in st.session_state:
+        st.session_state.deposition_id = 109575
 
-st.warning("missing option to enter APIkey")
-if report.valid:
-    control = portals.ZenodoControl(
+    st.warning("missing option to enter APIkey")
+    if report.valid:
+        control = portals.ZenodoControl(
             metafn=zenodo_path,
             apikey=st.secrets["apikey"],
             base_url="https://sandbox.zenodo.org/api/",
             deposition_id = st.session_state.deposition_id
         )
-    if st.button("upload"):
-        try:
-            st.session_state.deposition_id = st.session_state.package.publish(control=control)
-            st.write(st.session_state.deposition_id)
-        except Exception as e:
-            st.warning(e)
+        if st.button("upload"):
+            try:
+                st.session_state.deposition_id = st.session_state.package.publish(control=control)
+                st.write(st.session_state.deposition_id)
+            except Exception as e:
+                st.warning(e)
 
 
 ### option to merge tables by foreign keys
